3825b69730e170026661e855112859e8ef5e8bae
NiinoPepito <clemdrb24120@gmail.com>
2024-05-14 15:04:18 +0200

    le commit

---
+++ src/article/entity/article.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  title: string;
+
+  @Column({ type: 'varchar' })
+  content: string;
+
+  @Column({ type: 'varchar' })
+  author: string;
+}
