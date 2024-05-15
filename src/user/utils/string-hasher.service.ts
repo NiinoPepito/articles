c18edd31eb2e806cd285b87c34973cf95724dad5
NiinoPepito <clemdrb24120@gmail.com>
2024-05-15 11:09:25 +0200

    TP 3 update

--- src/user/utils/string-hasher.service.ts
+++ src/user/utils/string-hasher.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { User } from '../entity/user.entity';
-import { UserCreateDto } from '../dto/user-create.dto';
+import * as bcrypt from 'bcrypt';
 
 Injectable();
 export class StringHasherService {
 
   async stringHasher(password: string) {
     try {
-      const hash = await import('bcrypt').then(bcrypt => bcrypt.hash(password, 10));
+      const hash = await bcrypt.hash(password, 10);
 
       return hash;
     } catch (error) {
