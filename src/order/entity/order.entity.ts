38d48869835e7104ad59375bc835b67164ff204c
NiinoPepito <clemdrb24120@gmail.com>
2024-05-16 16:25:54 +0200

    update enum

--- src/order/entity/order.entity.ts
+++ src/order/entity/order.entity.ts
@@ -1,20 +1,17 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { OrderCreateDto } from "../dto/order-create.dto";
-import { ShippingUpdateDto } from "../dto/shipping-update.dto";
-import { InvoiceAdressUpdateDto } from "../dto/invoice-adress-update.dto";
-
-enum OrderStatus {
-  CREATED = 'Créée',
-  PAID = 'Payée',
-  CANCELED = 'Annulée'
-}
-// enum ShippingMethod {
-//   STANDARD = 'Standard',
-//   EXPRESS = 'Express',
-// }
+import { OrderCreateDto } from '../dto/order-create.dto';
+import { ShippingUpdateDto } from '../dto/shipping-update.dto';
+import { InvoiceAdressUpdateDto } from '../dto/invoice-adress-update.dto';
 
 @Entity()
 export class Order {
+  static OrderStatus = {
+    Cart: 'Cart',
+    Paid: 'Paid',
+    ShippingSet: 'ShippingSet',
+    InvoiceSet: 'InvoiceSet',
+    Cancelled: 'Cancelled',
+  };
 
   @PrimaryGeneratedColumn()
   id: number;
@@ -22,10 +19,10 @@ export class Order {
   @Column({ type: 'date' })
   createdAt: Date;
 
-  @Column({ type: 'date'})
+  @Column({ type: 'date' })
   updatedAt: Date;
 
-  @Column({ type: 'date' , nullable: true})
+  @Column({ type: 'date', nullable: true })
   paidAt: Date;
 
   @Column({ type: 'varchar' })
@@ -58,12 +55,12 @@ export class Order {
   constructor(createOrderData?: OrderCreateDto) {
     if (createOrderData) {
       if (createOrderData.items.length > 3) {
-        throw new Error('Le nombre d\'items ne peut pas dépasser trois.');
+        throw new Error("Le nombre d'items ne peut pas dépasser trois.");
       }
       this.createdAt = new Date();
       this.updatedAt = new Date();
       this.paidAt = null;
-      this.status = OrderStatus.CREATED;
+      this.status = Order.OrderStatus.Cart;
       this.customer = createOrderData.customer;
       this.items = createOrderData.items;
       this.total = 10 * createOrderData.items.length;
@@ -76,20 +73,26 @@ export class Order {
   }
 
   pay() {
-    if (this.status !== OrderStatus.CREATED) {
-      throw new Error('L\'order n\'est pas en cours de création');
+    if (this.status !== Order.OrderStatus.Cart) {
+      throw new Error("L'order n'est pas en cours de création");
     }
     this.paidAt = new Date();
-    this.status = OrderStatus.PAID;
+    this.status = Order.OrderStatus.Paid;
     this.updatedAt = new Date();
   }
 
-  updateShippingAdress(data : ShippingUpdateDto) {
+  updateShippingAdress(data: ShippingUpdateDto) {
     if (
-      this.status !== OrderStatus.CREATED &&
-      this.status !== OrderStatus.PAID
+      this.status !== Order.OrderStatus.Cart ||
+      this.status !== Order.OrderStatus.Paid
     ) {
-      throw new Error('L\'order n\'est pas en cours de création ou payée');
+      throw new Error("L'order n'est pas en cours de création ou payée");
+    }
+    if (
+      this.status != Order.OrderStatus.InvoiceSet ||
+      this.status != Order.OrderStatus.ShippingSet
+    ) {
+      this.status = Order.OrderStatus.ShippingSet;
     }
     this.shippingAddress = data.shippingAddress;
     this.shippingMethod = data.shippingMethod;
@@ -101,12 +104,15 @@ export class Order {
     }
   }
 
-  updateInvoiceAddress(data : InvoiceAdressUpdateDto) {
+  updateInvoiceAddress(data: InvoiceAdressUpdateDto) {
     if (
-      this.status !== OrderStatus.CREATED &&
-      this.status !== OrderStatus.PAID
+      this.status !== Order.OrderStatus.Cart ||
+      this.status !== Order.OrderStatus.Paid
     ) {
-      throw new Error('L\'order n\'est pas en cours de création ou payée');
+      throw new Error("L'order n'est pas en cours de création ou payée");
+    }
+    if (this.status != Order.OrderStatus.InvoiceSet) {
+      this.status = Order.OrderStatus.InvoiceSet;
     }
     this.invoiceAddress = data.invoiceAdress;
     this.invoiceAddressSetAt = new Date();
