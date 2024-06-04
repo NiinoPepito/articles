e3754e6470fa182e901903de59f017cc0db27a99
NiinoPepito <clemdrb24120@gmail.com>
2024-06-04 09:55:07 +0200

    OUI

--- src/order/controller/order.controller.ts
+++ src/order/controller/order.controller.ts
@@ -6,7 +6,7 @@ import {
   Param,
   ParseIntPipe,
   Post,
-  Put,
+  Put, UseGuards,
 } from '@nestjs/common';
 import { CreateOrderService } from '../use-case/create-order.service';
 import { OrderCreateDto } from '../dto/order-create.dto';
@@ -15,6 +15,7 @@ import { OrderUpdateShippingDto } from '../dto/order-update-shipping.dto';
 import { UpdateShippingOrderService } from '../use-case/update-shipping-order.service';
 import { UpdateInvoiceAddressOrderService } from '../use-case/update-invoice-address-order.service';
 import { OrderUpdateInvoiceAddressDto } from '../dto/order-update-invoice-address.dto';
+import { AuthGuard } from '../../auth/auth.guard';
 
 @Controller('orders')
 export class OrderController {
@@ -26,16 +27,19 @@ export class OrderController {
   ) {
   }
 
+  @UseGuards(AuthGuard)
   @Post()
   createOrder(@Body() data: OrderCreateDto) {
     return this.createOrderService.createOrder(data);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id/pay')
   updatePayOrder(@Param('id', ParseIntPipe) id: number) {
     return this.payOrderService.payOrder(id);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id/shipping')
   updateShippingAddress(
     @Param('id', ParseIntPipe) id: number,
@@ -44,6 +48,7 @@ export class OrderController {
     return this.updateShippingOrderService.updateShipping(id, data);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id/invoice-address')
   updateInvoiceAddress(
     @Param('id', ParseIntPipe) id: number,
